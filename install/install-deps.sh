c06183a7cff2cb41e0a334ab550aa7f4bb0a5497
Stéphane Goetz <onigoetz@onigoetz.ch>
2017-02-10 23:06:26 +0100

    Improve base installation & colors

--- install/install-deps.sh
+++ install/install-deps.sh
@@ -14,13 +14,13 @@ brew install caskroom/cask/brew-cask
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
 echo "Installing common applications"
-brew cask install google-chrome firefox mou filezilla imageoptim mysqlworkbench sourcetree cakebrew textmate atom
+brew cask install google-chrome firefox mou filezilla imageoptim mysqlworkbench sourcetree cakebrew textmate
 
-echo "Installing some more apps"
-brew cask install apache-directory-studio fantastical cord
+echo "Installing Quick Look Plugins"
+brew cask install webpquicklook qlimagesize qlprettypatch quicklook-json qlmarkdown qlcolorcode
 
 echo "Installing ZSH"
-brew install zsh coreutils
+brew install zsh coreutils zsh-syntax-highlighting
 sudo /bin/sh -c 'echo "/usr/local/bin/zsh" >> /etc/shells'
 chsh -s /usr/local/bin/zsh
 
@@ -33,33 +33,34 @@ echo "Installing PHP"
 brew tap josegonzalez/php
 brew tap homebrew/dupes
 
-brew install php55 --without-apache --with-debug --with-fpm --with-pgsql
-ln -sfv /usr/local/opt/php55/*.plist ~/Library/LaunchAgents
-brew install php55-mcrypt php55-xdebug php55-xhprof
-
-echo "Installing HHVM"
-brew cask install xquartz
-brew tap mcuadros/homebrew-hhvm
-brew install hhvm --with-mariadb
+#brew install php56 --without-apache --with-debug --with-fpm --with-postgresql
+#ln -sfv /usr/local/opt/php56/*.plist ~/Library/LaunchAgents
+#brew install php56-mcrypt php56-xdebug php56-xhprof
 
 echo "Installing Mysql"
 brew install mariadb
 ln -sfv /usr/local/opt/mariadb/*.plist ~/Library/LaunchAgents
 
-echo "Installing Nginx"
-#brew tap homebrew/nginx
-#brew install nginx-full
-brew install nginx
-ln -sfv /usr/local/opt/nginx/*.plist ~/Library/LaunchAgents
+## @ Home
+######################################################################
 
-echo "Installing Drupal Stuff"
-brew install postgresql
-ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
-brew cask install pgadmin3
+echo "Installing home apps"
+brew cask install a-better-finder-rename calibre cleanmymac dropbox hubic skype teamviewer tower transmit vlc
+
+# Plex media player
+#
+
+## @ Work
+######################################################################
+
+echo "Installing some more apps"
+brew cask install apache-directory-studio
 
 echo "Installing Java versions"
 brew install terminal-notifier
-brew install https://raw.githubusercontent.com/entrypass/jenv/homebrew/homebrew/jenv.rb
+brew install jenv
 eval "$(jenv init -)"
-jenv add /Library/Java/JavaVirtualMachines/jdk1.8.0_25.jdk/Contents/Home
-jenv add /Library/Java/JavaVirtualMachines/jdk1.7.0_71.jdk/Contents/Home
+jenv add `/usr/libexec/java_home -v 1.7`
+jenv add `/usr/libexec/java_home -v 1.8`
+jenv enable-plugin maven
+jenv rehash
