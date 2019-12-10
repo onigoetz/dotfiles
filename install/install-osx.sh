25e5329fcbb47961f49f42a0620860e7270a9ed4
Stéphane Goetz <onigoetz@onigoetz.ch>
2019-12-10 22:05:34 +0100

    Configuration tweaks

--- install/install-osx.sh
+++ install/install-osx.sh
@@ -7,48 +7,32 @@ echo "Installing Homebrew..."
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "Installing Common softwares ..."
-brew install curl dos2unix graphviz mercurial node wget git
+brew install curl dos2unix graphviz wget git
 
-echo "Installing Homebrew Cask..."
-brew install caskroom/cask/brew-cask
+echo "Configuring Homebrew Cask..."
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
 echo "Installing common applications"
-brew cask install google-chrome firefox mou filezilla imageoptim mysqlworkbench sourcetree cakebrew textmate
+brew cask install google-chrome firefox mageoptim
 
 echo "Installing Quick Look Plugins"
-brew cask install webpquicklook qlimagesize qlprettypatch quicklook-json qlmarkdown qlcolorcode
+brew cask install webpquicklook qlprettypatch quicklook-json qlmarkdown qlcolorcode
 
 echo "Installing ZSH"
 brew install zsh coreutils
 sudo /bin/sh -c 'echo "/usr/local/bin/zsh" >> /etc/shells'
 chsh -s /usr/local/bin/zsh
 
-echo "Installing some fonts"
-/bin/sh ./fonts/install.sh
-
-## TODO :: install Solarized Dark Terminal.app theme and set as default
-
-echo "Installing PHP"
-brew tap josegonzalez/php
-brew tap homebrew/dupes
-
-#brew install php56 --without-apache --with-debug --with-fpm --with-postgresql
-#ln -sfv /usr/local/opt/php56/*.plist ~/Library/LaunchAgents
-#brew install php56-mcrypt php56-xdebug php56-xhprof
-
-echo "Installing Mysql"
-brew install mariadb
-ln -sfv /usr/local/opt/mariadb/*.plist ~/Library/LaunchAgents
+## TODO :: install Fira Code
+## TODO :: install Hyperblue.app theme and set as default
 
 ## @ Home
 ######################################################################
 
 echo "Installing home apps"
-brew cask install a-better-finder-rename calibre cleanmymac dropbox hubic skype teamviewer tower transmit vlc
+brew cask install a-better-finder-rename cleanmymac dropbox skype teamviewer tower vlc brave-browser github gitkraken
 
-# Plex media player
-#
+# TODO  Plex media player
 
 ## @ Work
 ######################################################################
